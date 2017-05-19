8d1b6c76d3ef1046b89c851a0cc11e9fcf62fc1e
Jared Windover <jaredwindover@gmail.com>
2017-05-19 15:22:44 -0400

    Updating to later rest

--- src/lib/rest-client.js
+++ src/lib/rest-client.js
@@ -4,10 +4,12 @@ import rest from 'rest';
 import defaultRequest from 'rest/interceptor/defaultRequest';
 import mime from 'rest/interceptor/mime';
 import errorCode from 'rest/interceptor/errorCode';
+import template from 'rest/interceptor/template';
 
 const client = rest
-  .wrap(defaultRequest, { headers: { 'User-Agent': 'Cronofy Node' } })
-  .wrap(mime, { mime: 'application/json' })
-  .wrap(errorCode);
+			.wrap(template)
+			.wrap(defaultRequest, { headers: { 'User-Agent': 'Cronofy Node' } })
+			.wrap(mime, { mime: 'application/json' })
+			.wrap(errorCode);
 
 export default client;
